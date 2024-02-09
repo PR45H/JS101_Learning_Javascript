6a977859142170bfeb4d363338ab872206862811
PR45H <Prashantpillai7@gmail.com>
2024-02-10 00:25:04 +0530

    initial commit

---
+++ mathematical_problem4.js
@@ -0,0 +1,12 @@
+// Given 3 numbers (all different values), print which is greatest
+
+let a=5;
+let b=6;
+let c=7;
+if(a>b && a>c){
+  console.log("a is greatest");
+}else if(b>a && b>c){
+  console.log("b is greatest");
+}else{
+  console.log("c is greatest");
+}
